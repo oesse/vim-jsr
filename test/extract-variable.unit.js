fe17633cefd8746bab45a8773c984a44d305583c
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2017-05-07 19:31:02 +0200

    Support variable extraction out of function argument.

---
+++ test/extract-variable.unit.js
@@ -0,0 +1,20 @@
+import * as acorn from 'acorn'
+import escodegen from 'escodegen'
+import fs from 'fs'
+import path from 'path'
+
+import * as refactor from '../lib'
+
+import { expect } from 'chai'
+
+describe('extractVariable', () => {
+  context('out of function argument', () => {
+    const sourceCode = 'doImportantStuff(1, a + b)'
+
+    it('returns range of lines and source code with extracted variable', () => {
+      const charRange = [20, 25]
+      const { refactoredCode } = refactor.extractVariableFromRange(sourceCode, charRange, 'varName')
+      expect(refactoredCode).to.equal('const varName = a + b;\ndoImportantStuff(1, varName);')
+    })
+  })
+})
